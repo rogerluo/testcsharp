712d436cc42aec395e3bc39cf1a69ac8c3092a6f
rogerluo <luodingjia1985@163.com>
2013-09-25 07:55:15 +0800

    add sharpssh

---
+++ testSharpSSH/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace testSharpSSH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Tamir.SharpSSH.dll"));
+            assembly.get
+        }
+    }
+}
